80b999cf4c1498da28a2246d0d0d6ce8935b5c43
breidt-USDA <fred.breidt@usda.gov>
2022-12-17 16:51:48 -0500

    Initial commit

---
+++ source/FLR.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = FLR(inputArg1,inputArg2)
+%FLR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
