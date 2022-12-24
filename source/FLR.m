8e134b0a6e35e86de46901f6b2df05e56dc39fd5
breidt-USDA <fred.breidt@usda.gov>
2022-12-24 10:24:50 -0500

    Added documentation after testing

--- source/FLR.m
+++ source/FLR.m
@@ -1,9 +1,18 @@
 function res = FLR(lac,ace,tempC,post_mat,perc)
-%FLR Summary of this function goes here
-%   Detailed explanation goes here
+%Process a single data set with lactic, acetic and tempC
+%INPUT:
+%  lac = lactic (mM)
+%  ace = acetic (mM)
+%  tempC = temperature C
+%  post_mat = Quentin's matrix
+%  perc = quantile val
+%OUTPUT
+%  res = quantile result
+%--------------------------------------------------------------------------
+%first make col vec with data, and cross products
 colvec = [1;lac;ace;tempC;lac*ace;lac*tempC;ace*tempC];
-resvec = post_mat * colvec;
-epred = exp(resvec);
-res = quantile(epred,perc);
+resvec = post_mat * colvec;               %multiply by matrix
+epred = exp(resvec);                      %convert vec vals to e^x
+res = quantile(epred,perc);               %calc quantile
 end
 
