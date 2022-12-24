58c6800496e707fad2362d728bfbf348e7df9cd3
breidt-USDA <fred.breidt@usda.gov>
2022-12-24 08:52:24 -0500

    Wrote basic algorithm, needs input from Quentin

--- source/FLR.m
+++ source/FLR.m
@@ -1,7 +1,9 @@
-function [outputArg1,outputArg2] = FLR(inputArg1,inputArg2)
+function res = FLR(lac,ace,tempC,post_mat,perc)
 %FLR Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+colvec = [1;lac;ace;tempC;lac*ace;lac*tempC;ace*tempC];
+resvec = post_mat * colvec;
+epred = exp(resvec);
+res = quantile(epred,perc);
 end
 
